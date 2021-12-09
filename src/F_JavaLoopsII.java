7659d27d848b11fa1211c0350bd6f5a0b86c3d7c
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-12-09 19:49:55 -0300

    Novo desafio

---
+++ src/F_JavaLoopsII.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class F_JavaLoopsII{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int aux = a;
+
+            for (int j = 0; j < n ; j++){
+                aux = aux + b * (int)Math.pow(2, j);
+                System.out.print(aux + " ");
+            }
+
+            System.out.println();
+
+        }
+        in.close();
+    }
+}
