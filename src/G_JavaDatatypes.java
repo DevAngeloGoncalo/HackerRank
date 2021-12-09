b0a54f1fdaed62e35325122db466c7151736e876
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2021-12-09 20:13:00 -0300

    Alterações

--- src/G_JavaDatatypes.java
+++ src/G_JavaDatatypes.java
@@ -1,14 +1,11 @@
 import java.util.*;
-import java.io.*;
 
 class Solution{
     public static void main(String []argh)
     {
-
-
-
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
+        sc.close();
 
         for(int i=0;i<t;i++)
         {
